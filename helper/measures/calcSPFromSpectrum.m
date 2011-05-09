200baebff724baa6b1b35e02a1bf3c650533a8b6
Jan Winter <j.winter@tu-berlin.de>
2011-05-09 13:26:17 +0200

    SP ratio added

---
+++ helper/measures/calcSPFromSpectrum.m
@@ -0,0 +1,11 @@
+function SP = calcSPFromSpectrum(spectrum)
+
+lambda_i = linspace(380,780,401);
+V_strich_i = interp1(lambda_CIE, V, lambda_i);
+Lv_photopic = 683 * sum(V_i .* spectrum);
+
+lambda_i = linspace(380,780,401);
+V_strich_i = interp1(lambda_CIE, V_strich, lambda_i);
+Lv_scotopic = 1758 * sum(V_strich_i .* spectrum);
+
+SP = Lv_scotopic / Lv_photopic;
