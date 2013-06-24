356d093952c18d53e9c232a7b12a5ef634b7d6ea
Sandy Buschmann <16900309523@mailbox.tu-berlin.de>
2013-06-24 14:16:02 +0200

    HowTo Doc

--- evaluateDataset_batch.m
+++ evaluateDataset_batch.m
@@ -4,79 +4,15 @@
 
 evaluateBatchSet = {
 
-<<<<<<< HEAD
-'/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Weefstr_Amb_R2', 'LOWER_EDGE';
-%        '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/TreskowStr/Treskowstr_LED_gemessen', 'RP800';
-=======
-       '/Users/robert/Desktop/Development/LMK/LMK_Data_evaluation/database/dataset/Treskowstr_R3', 'LOWER_EDGE';
->>>>>>> be03a26d2c19f76f0cd432db82fb9a8ff1acdd3b
-%        '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/TreskowStr/Treskowstr_LED_gemessen', 'LOWER_EDGE';
-%        '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/TreskowStr/Treskowstr_LED_gemessen', 'STRONGEST_EDGE';
-%        '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/TreskowStr/Treskowstr_LED_gemessen', 'STRONGEST_CORNER';
-%        '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/TreskowStr/Treskowstr_LED_gemessen', '2DEGREE_BACKGROUND';
+% '/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'RP800';
+'/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'LOWER_EDGE'
+% '/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'LEFT_EDGE';
+% '/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'RIGHT_EDGE';
+% '/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'UPPER_EDGE';
+% '/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'STRONGEST_EDGE';
+% '/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'STRONGEST_CORNER';
+% '/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', '2DEGREE_BACKGROUND'
        
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L1_Mitte', 'RP800';
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L2_Mitte', 'RP800';
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L3_Mitte', 'RP800';
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L4_Mitte', 'RP800';
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L5_Mitte', 'RP800';
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L6_Mitte', 'RP800';
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L7_Mitte', 'RP800';
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L8_Mitte', 'RP800';
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L9_Mitte', 'RP800';
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L10_Mitte', 'RP800';
-    
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L1_Rand',
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L2_Rand',
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L3_Rand',
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L4_Rand',
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L5_Rand',
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L6_Rand',
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L7_Rand',
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L8_Rand',
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L9_Rand',
-%     '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/FahrradleuchtenKeller/L10_Rand'
-    
-     %'/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/LVKOptimierung/Schade_Optimierung_E',
-     %'/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/LVKOptimierung/Schade_Optimierung_L'
-     %'/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/TreskowStr/Treskowstr_HS_RP8_1035',
-    % '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/TreskowStr/Treskowstr_HS_RP8_1449',
-   %  '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_HS_RP8_2070',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_gemessen',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_R3_2070',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_4',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_6',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_10',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/databa
-%      se/datasets/Treskowstr_LED_RP8_20',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_30',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_40',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_50',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_60',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_70',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_80',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_90',
-%      '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_100',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_4_dark',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_4_dim',
-
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_4_dim_ns',
- %    '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_4_ds',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_4_ds_nlbmf',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_4_undim',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_4_undim_ns',
- %   '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_1035',
-  %  '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_1449',
-  %  '/Users/jw/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_2070',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_dark',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_dim',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_dim_ns',
- %    '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_ds'
-
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_undim',
- %    '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_LED_RP8_undim_ns',
-%     '/Users/janwinter/Desktop/Development/LMK/LMK_Data_evaluation/database/datasets/Treskowstr_test'
-    
     };
 
 
