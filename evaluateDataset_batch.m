9ef44e45a42842e3b0b6caa7a3c0460581d6baa6
Jan Winter <j.winter@tu-berlin.de>
2013-11-27 11:54:14 +0100

    bugfixes

--- evaluateDataset_batch.m
+++ evaluateDataset_batch.m
@@ -6,8 +6,30 @@
 
 evaluateBatchSet = {
     
-'d:/cygwin/home/Robert/GitUmgebung/LMK/LMK_Data_Evaluation/database/dataset/PseudoTest', 'RP800';
-%'/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'LOWER_EDGE';
+%'d:/cygwin/home/Robert/GitUmgebung/LMK/LMK_Data_Evaluation/database/dataset/PseudoTest', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/C2W3/CELEST_55W/CELEST_55W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/C2W3/CIVIC_100W/CIVIC_100W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/C2W3/DECOSTREET_70W/DECOSTREET_70W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/C2W3/INDRA_33W/INDRA_33W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/C2W3/JET_42W/JET_42W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/C2W3/LEMNIS_70W/LEMNIS_70W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/C2W3/ORACLES_100W/ORACLES_100W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/C2W3/RIGA_70W/RIGA_70W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/C2W3/VICTOR_40W/VICTOR_40W', 'RP800';
+
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/R3/CELEST_55W/CELEST_55W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/R3/CIVIC_100W/CIVIC_100W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/R3/DECOSTREET_70W/DECOSTREET_70W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/R3/INDRA_33W/INDRA_33W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/R3/JET_42W/JET_42W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/R3/LEMNIS_70W/LEMNIS_70W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/R3/ORACLES_100W/ORACLES_100W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/R3/RIGA_70W/RIGA_70W', 'RP800';
+'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/R3/VICTOR_40W/VICTOR_40W', 'RP800';
+
+%'/Users/Robert/Desktop/Development/LMK/LMK_Data_Evaluation/database/datasets/DYANA_150W_DEFEKT/DYANA_150W', 'RP800';
+
+
 %'/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'LEFT_EDGE';
 % '/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'RIGHT_EDGE';
 % '/Users/sandy/Desktop/Development/LMK_Data_evaluation/database/StreetXY/StreetXY', 'UPPER_EDGE';
